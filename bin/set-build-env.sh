51d2969df6d7e317fc6291248c5bceceb722d4e0
Steve Pillinger <Steve.Pillinger@gmail.com>
2018-07-19 06:52:21 +0100

    Added set-build-env.sh to aid debugging

---
+++ bin/set-build-env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DEST=/home/development/n-build/src/HandBrake/build/contrib
+
+export PATH=/home/development/CT/x86_64-QNAP-linux-gnu/cross-tools/bin:/bin:/usr/bin:${DEST}/bin
+
+export LD_LIBRARY_PATH=${DEST}/lib
+
+export LDFLAGS=-L${DEST}/lib
+
+export CFLAGS=-I${DEST}/include
+
+export PKG_CONFIG_PATH=${DEST}/lib/pkgconfig
+
+  # For libpvx
+
+export CROSS=x86_64-QNAP-linux-gnu-
